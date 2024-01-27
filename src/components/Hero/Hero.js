b2367eafcb31753558f96852fcc6c07b6fa3292c
jacky-ui <jacky.du.004@gmail.com>
2024-01-27 11:21:08 -0500

    create typing effect with css instead of useState

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,9 +8,25 @@ import codingBg from '../../assets/images/coding-background-v2.png';
 
 function Hero() {
     const [ currentTitle, nextCurrentTitle ] = useState('Front-End Developer');
+
+    if(currentTitle === 'Front-End Developer') {
+        setTimeout(() => {
+            nextCurrentTitle('Full-Stack Developer')
+        }, 5000);
+    } else if(currentTitle === 'Full-Stack Developer') {
+        setTimeout(() => {
+            nextCurrentTitle('Front-End Developer')
+        })
+    }
+
     return(
-        <>
-        </>
+        <div className='hero' id='home'>
+            <h1 className='hero__text'>
+                Jacky Du, C. Tech<br/> 
+                {/*<span>{currentTitle}</span>*/}
+                <span></span>
+            </h1>
+        </div>
     )
 };
 
