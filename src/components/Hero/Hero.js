595e86a0c2b5204d64b4736d88e66fc8a60b659c
jacky-ui <jacky.du.004@gmail.com>
2024-01-27 11:02:40 -0500

    import avatar and coding background into hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,10 @@
 // import styles and components
 import './Hero.scss';
 
+// import assets
+import avatarImage from '../../assets/images/avataaars.svg';
+import codingBg from '../../assets/images/coding-background-v2.png';
+
 function Hero() {
     return(
         <>
