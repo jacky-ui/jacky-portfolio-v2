e121529ca84b7201e1e30b8cb865f6561691f62a
jacky-ui <jacky.du.004@gmail.com>
2024-01-27 11:21:57 -0500

    delete react useState and function since no longer needed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,4 @@
 // import react, styles and components
-import React, { useState } from 'react';
 import './Hero.scss';
 
 // import assets
@@ -7,18 +6,6 @@ import avatarImage from '../../assets/images/avataaars.svg';
 import codingBg from '../../assets/images/coding-background-v2.png';
 
 function Hero() {
-    const [ currentTitle, nextCurrentTitle ] = useState('Front-End Developer');
-
-    if(currentTitle === 'Front-End Developer') {
-        setTimeout(() => {
-            nextCurrentTitle('Full-Stack Developer')
-        }, 5000);
-    } else if(currentTitle === 'Full-Stack Developer') {
-        setTimeout(() => {
-            nextCurrentTitle('Front-End Developer')
-        })
-    }
-
     return(
         <div className='hero' id='home'>
             <h1 className='hero__text'>
