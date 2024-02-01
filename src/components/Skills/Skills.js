be3937440c74dbdbb8f077c62ccb57e0e8afa5eb
jacky-ui <jacky.du.004@gmail.com>
2024-01-31 21:48:05 -0500

    basic styling for skills section and creat boiler folder and file for skills card

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,9 +1,12 @@
 // Style, and component imports
+import SkillsCard from '../SkillsCard/SkillsCard';
+import './Skills.scss';
 
 function Skills() {
     return(
-        <>
-        </>
+        <section className="skills">
+            <SkillsCard />
+        </section>
     )
 };
 
---
+++ src/components/SkillsCard/SkillsCard.js
@@ -0,0 +1,11 @@
+// style, component and constant imports
+import './SkillsCard.scss';
+
+function SkillsCard() {
+    return(
+        <>
+        </>
+    )
+};
+
+export default SkillsCard;
