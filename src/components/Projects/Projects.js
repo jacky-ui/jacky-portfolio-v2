34cb15c1174658ee743b0bd48141bde1eb4c7ddf
jacky-ui <jacky.du.004@gmail.com>
2024-02-06 17:20:39 -0500

    import projectcard into projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,14 @@
 // style, component and constant imports
+import ProjectCard from '../ProjectCard/ProjectCard';
 import './Projects.scss';
 
 function Projects() {
     return(
         <section className='projects'>
             <h2>My Recent Projects</h2>
+            <div>
+                <ProjectCard />
+            </div>
         </section>
     )
 };
