a2ec892f64b3107c4998bf3896369e456466bfcc
jacky-ui <jacky.du.004@gmail.com>
2024-02-05 21:29:38 -0500

    forgot to remove old code

--- src/components/SkillsCard/SkillsCard.js
+++ src/components/SkillsCard/SkillsCard.js
@@ -26,66 +26,6 @@ function SkillsCard() {
                     </ul>
                 </div>
             ))}
-            {/*<div className='skillscard__container'>
-                <h3>Front End Skills</h3>
-                <ul className='skillscard__list'>
-                    <li>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                        <p>HTML</p>
-                    </li>
-                    <li>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                        <p>HTML</p>
-                    </li>
-                    <li>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                        <p>HTML</p>
-                    </li>
-                    <li>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                        <p>HTML</p>
-                    </li>
-                    <li>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                        <p>HTML</p>
-                    </li>
-                </ul>
-            </div>
-            <div className='skillscard__container'>
-                <h3>Front End Skills</h3>
-                <ul className='skillscard__list'>
-                    <li>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                        <p>HTML</p>
-                    </li>
-                    <li>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                        <p>HTML</p>
-                    </li>
-                    <li>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                        <p>HTML</p>
-                    </li>
-                    <li>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                        <p>HTML</p>
-                    </li>
-                    <li>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                        <p>HTML</p>
-                    </li>
-                </ul>
-            </div>
-            <div className='skillscard__container column'>
-                <h3>Libraries, Tools, and Others</h3>
-                <ul className='skillscard__list'>
-                    <li>something</li>
-                    <li>something</li>
-                    <li>something</li>
-                    <li>something</li>
-                    <li>something</li>
-                </ul>
-            </div>*/}
         </article>
     )
 };
