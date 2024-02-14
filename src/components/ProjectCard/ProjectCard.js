71905cfc9be0b8c30c811319223f7158d8f3a888
jacky-ui <jacky.du.004@gmail.com>
2024-02-14 10:24:31 -0500

    fix ease in and out animation

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -12,14 +12,16 @@ function ProjectCard() {
                     src={testing}
                     alt=''
                 />
-            </article>
-            <article className='projectCard'>
-                <img
-                    className='projectCard__img' 
-                    src={testing}
-                    alt=''
-                />
-            </article>
+                <figcaption className='projectCard__caption'>
+                    <h3>Studio Ghibli API</h3>
+                    <p>somethig something fdjsfadsklf fdsjfsdl fdskjfal fdjskfjlas fjdkfljsal fjdskfjslda</p>
+                    <div className='projectCard__caption--flex'>
+                        <a href='www.google.com'>Deployed</a>
+                        <a href='www.google.com'>Github</a>
+                    </div>
+                    <p>#html #css #sass #javascript</p>
+                </figcaption>
+            </article>    
             <article className='projectCard'>
                 <img
                     className='projectCard__img' 
@@ -35,7 +37,71 @@ function ProjectCard() {
                     </div>
                     <p>#html #css #sass #javascript</p>
                 </figcaption>
-            </article>                        
+            </article>  
+            <article className='projectCard'>
+                <img
+                    className='projectCard__img' 
+                    src={testing}
+                    alt=''
+                />
+                <figcaption className='projectCard__caption'>
+                    <h3>Studio Ghibli API</h3>
+                    <p>somethig something fdjsfadsklf fdsjfsdl fdskjfal fdjskfjlas fjdkfljsal fjdskfjslda</p>
+                    <div className='projectCard__caption--flex'>
+                        <a href='www.google.com'>Deployed</a>
+                        <a href='www.google.com'>Github</a>
+                    </div>
+                    <p>#html #css #sass #javascript</p>
+                </figcaption>
+            </article>  
+            <article className='projectCard'>
+                <img
+                    className='projectCard__img' 
+                    src={testing}
+                    alt=''
+                />
+                <figcaption className='projectCard__caption'>
+                    <h3>Studio Ghibli API</h3>
+                    <p>somethig something fdjsfadsklf fdsjfsdl fdskjfal fdjskfjlas fjdkfljsal fjdskfjslda</p>
+                    <div className='projectCard__caption--flex'>
+                        <a href='www.google.com'>Deployed</a>
+                        <a href='www.google.com'>Github</a>
+                    </div>
+                    <p>#html #css #sass #javascript</p>
+                </figcaption>
+            </article>  
+            <article className='projectCard'>
+                <img
+                    className='projectCard__img' 
+                    src={testing}
+                    alt=''
+                />
+                <figcaption className='projectCard__caption'>
+                    <h3>Studio Ghibli API</h3>
+                    <p>somethig something fdjsfadsklf fdsjfsdl fdskjfal fdjskfjlas fjdkfljsal fjdskfjslda</p>
+                    <div className='projectCard__caption--flex'>
+                        <a href='www.google.com'>Deployed</a>
+                        <a href='www.google.com'>Github</a>
+                    </div>
+                    <p>#html #css #sass #javascript</p>
+                </figcaption>
+            </article>  
+            <article className='projectCard'>
+                <img
+                    className='projectCard__img' 
+                    src={testing}
+                    alt=''
+                />
+                <figcaption className='projectCard__caption'>
+                    <h3>Studio Ghibli API</h3>
+                    <p>somethig something fdjsfadsklf fdsjfsdl fdskjfal fdjskfjlas fjdkfljsal fjdskfjslda</p>
+                    <div className='projectCard__caption--flex'>
+                        <a href='www.google.com'>Deployed</a>
+                        <a href='www.google.com'>Github</a>
+                    </div>
+                    <p>#html #css #sass #javascript</p>
+                </figcaption>
+            </article>                                                                                  
         </>
     )
 };
